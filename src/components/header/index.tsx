c160c97474aae5d7bf3249d48ab0a13193b10bba
USER <nakterankanogames@gmail.com>
2024-04-13 19:35:31 +0200

    navbar and header styling works ok (only desktop)

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,7 +5,7 @@ import Banner from "../../assets/baner.png"
 export default function Header() {
     return (
         <header id="pageHeader">
-            <img src={Banner} alt="Banner" id="mainBanner" />
+            <a href="/"><img src={Banner} alt="Banner" id="mainBanner"/></a>
         </header>
     )
 }
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,6 +3,8 @@ import './style.scss'
 import {Navbar, Nav, NavDropdown} from "react-bootstrap"
 import icon_pokeball from "../../assets/icon_pokeball.png"
 import icon_gear from "../../assets/icon_gear.png"
+import icon_games from "../../assets/icon_games.png"
+//Todo: find and import forum and other icons
 import Container from "../Container";
 
 export default function NavBar() {
@@ -29,16 +31,16 @@ export default function NavBar() {
             title: "Pokemon data",
             shortTitle: "Data",
             items: [
-                { text: "Pokedex", href: "#", type:"link"},
-                { text: "Moves", href: "#", type:"link" },
-                { text: "Type chart", href: "#", type:"link" },
-                { text: "Abilities", href: "#", type:"link" },
-                { text: "Items", href: "#", type:"link" },
-                { text: "Evolution chains", href: "#", type:"link" },
-                { text: "Pokemon locations", href: "#", type:"link" },
-                { text: "Sprite gallery", href: "#", type:"link" },
-                { text: "Quick links", href:"#", type:"text"} ,
-                { text: "National pokedex", href: "#", type:"link" }
+                {text: "Pokedex", href: "/pokedex", type:"link"},
+                {text: "Moves", href: "/move", type:"link" },
+                {text: "Type chart", href: "/type", type:"link" },
+                {text: "Abilities", href: "/ability", type:"link" },
+                {text: "Items", href: "/item", type:"link" },
+                {text: "Evolution chains", href: "/evolution", type:"link" },
+                {text: "Pokemon locations", href: "/location", type:"link" },
+                {text: "Sprite gallery", href: "/sprites", type:"link" },
+                {text: "Quick links", href:"#", type:"text"} ,
+                {text: "National pokedex", href: "/pokedex/national", type:"link" }
             ]
 
         },
@@ -53,19 +55,53 @@ export default function NavBar() {
                 {text: "Effort values (EVs)", href: '#', type:"link"},
                 {text: "Natures", href: '#', type:"link"},
                 {text: "IVs/Personality", href: '#', type:"link"},
+                {text: "Useful tools", href:"#", type:"text"} ,
                 {text: "Moveset searcher", href: '#', type:"link"},
                 {text: "Type coverage calculator", href: '#', type:"link"},
                 {text: "Text list generator", href: '#', type:"link"}
             ]
+        },
+        {
+            image: {icon_games},
+            title: "Pokemon games",
+            shortTitle: "Games",
+            items:[
+                {text: "Gen 2", href: "#", type: "text"},
+                {text: "Crystal", href: "#", type: "link"},
+                {text: "Gold/Silver", href: "#", type: "link"},
+                {text: "Gen 1", href: "#", type: "text"},
+                {text: "Yellow", href: "#", type: "link"},
+                {text: "Red/Blue", href: "#", type: "link"}
+            ]
+        },
+        {
+            image: {}, //todo: add the image here too
+            title: "Community",
+            shortTitle: "Community",
+            items:[
+                {text: "Pokemon Q&A", href: "#", type: "link"},
+                {text: "Pokemon Rate My Team", href: "#", type: "link"},
+                {text: "Meta(Suggestions)", href: "#", type: "link"}
+            ]
+        },
+        {
+            image: {}, //todo: add the image here also
+            title: "Other",
+            shortTitle: "Other",
+            items:[
+                {text: "Pokemon News", href: "#", type: "link"},
+                {text: "Maps/Puzzles", href: "#", type: "link"},
+                {text: "About/Contact Us", href: "#", type: "link"}
+            ]
         }
     ]
-    console.log({icon_gear})
+
     return (
         <Navbar expand={false}>
             <Nav id={"navbar"}>
                 {menuItems.map((menu,index)=>(
                     <Container key={index}>
-                        <img src={Object.values(menu.image)[0]} className="dropMenuIcon"/>
+                        <img src={Object.values(menu.image)[0]} className="dropMenuIcon" alt="icon"/>
                         <NavDropdown key={index} title={handleTitle(menu.title, menu.shortTitle)} className={"hoverDropdown"} renderMenuOnMount={true}>
                             {menu.items.map((item,idx)=>(
                                 <NavDropdown.Item key={idx} href={item.href} className={item.type}>
@@ -75,10 +111,9 @@ export default function NavBar() {
                         </NavDropdown>
                     </Container>
                 ))}
+                <div id="#searchbar">search</div>
             </Nav>
         </Navbar>
     )
 
 }
-
-//{Object.values(menu.image)[0]}
